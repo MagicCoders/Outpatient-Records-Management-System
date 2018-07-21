c260abc710c7a38de0099482d6657bbfbaea32e6
Code-Buddie <michaelmakali5@gmail.com>
2018-07-21 10:04:28 +0300

    Database service added

--- src/main/java/com/test/records/controllers/DoctorController.java
+++ src/main/java/com/test/records/controllers/DoctorController.java
@@ -1,12 +1,22 @@
 package com.test.records.controllers;
 
+import com.test.records.crud.PatientRepository;
+import com.test.records.models.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class DoctorController {
+
+    @Autowired
+    private PatientRepository patientRepository;
 
     @RequestMapping("/doctor")
     @ResponseBody
@@ -14,17 +24,41 @@ public class DoctorController {
         return "doctor content";
     }
 
-    @RequestMapping("/doctor/history/")
+    @RequestMapping("/doctor/history/{id}")
     @ResponseBody
-    public String doctorDetails(){
-        return "doctor content";
+    public Optional<Patient> doctorDetails( @PathVariable String id ){
+        Optional<Patient> patient = patientRepository.findById(id);
+        return patient;
     }
 
-    @RequestMapping(value = "/doctor/{id}")
+    @RequestMapping("/doctor/{id}")
     @ResponseBody
-    public String patientDetails(@PathVariable int id){
-        return "patient-details";
+    public Optional<Patient> patientDetails( @PathVariable String id ){
+        Optional<Patient> patient = patientRepository.findById(id);
+        return patient;
     }
 
-    //Add post request and put request methods
+    @PostMapping("/doctor")
+    public ResponseEntity<Void> createPatientRecord (@RequestBody Patient patient){
+        patientRepository.save(patient);
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/doctor{id}")
+    public ResponseEntity<Patient> updatePatientRecord(@PathVariable String id, @RequestBody Patient patient){
+        Optional<Patient> currentPatient = patientRepository.findById(id);
+
+        if(currentPatient.isPresent()){
+            Patient thePatient = currentPatient.get();
+            thePatient.setTests(patient.getTests());
+            thePatient.setDiagnosis(patient.getDiagnosis());
+            thePatient.setPrognosis(patient.getPrognosis());
+
+            patientRepository.save(thePatient);
+            return new ResponseEntity<Patient>(thePatient, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<Patient>(HttpStatus.NOT_FOUND);
+    }
 }

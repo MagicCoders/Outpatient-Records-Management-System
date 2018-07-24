0adceb551afeeac5c3e8296b62c5aeaf64ad73b2
ogaroh <ogaroerick1998@gmail.com>
2018-07-24 15:16:05 +0300

    Updated User Interface

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Login from "./Login";
 
 class App extends Component {
   render() {
 
       // 'let' keyword allows the declared variable to mutate/change.
-      const testVariable = 'First App, nothing interesting here.';
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hacker News: Powered by React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.(hot reload)
-        </p>
-          <h2>{testVariable}</h2>
-          <p> </p>
-        <p className="App-footer">
-          Copyright &copy; <br/> Erick Ogaro, 2018
-        </p>
-      </div>
-
-    );
+      let testVariable = '"Quality Service Delivery"';
+      return <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h1 className="App-title">Log In</h1>
+          </header>
+          <p className="App-intro">
+              Submit your details below to log in:
+          </p>
+          <p className="App-body">
+          </p>
+          <p>{testVariable}</p>
+          <p className="App-footer">
+              Copyright &copy; 2018 <br/> Egerton University Sanatorium
+          </p>
+      </div>;
   }
 }
 

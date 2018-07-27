d5d2761f3db103b9923968bf35e9f93ea34bf165
wesleigh <wkijedi@gmail.com>
2018-07-27 12:34:08 +0300

    getters and setters on users

--- src/main/java/com/test/records/data/StudentInfo.java
+++ src/main/java/com/test/records/data/StudentInfo.java
@@ -3,4 +3,19 @@ public class StudentInfo {
     private String name;
     private String id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
